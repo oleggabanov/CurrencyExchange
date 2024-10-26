b6784291dd3c1b96d1fa4e31bbf6e794d9b69989
@oleggabanov <oleg.gabanov@bk.ru>
2024-10-27 00:51:37 +0300

    try to send json response

--- src/main/java/com/move/servlet/CurrenciesController.java
+++ src/main/java/com/move/servlet/CurrenciesController.java
@@ -1,26 +1,26 @@
 package com.move.servlet;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.move.model.CurrencyResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/")
+@WebServlet("/currencies")
 public class CurrenciesController extends HttpServlet {
+
+  private ObjectMapper mapper = new ObjectMapper();
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    resp.setContentType("application/json");
+    CurrencyResponse currencyResponse = new CurrencyResponse(1, "RUB", "Russian Ruble", "₽");
+    mapper.writeValue(resp.getOutputStream(), currencyResponse);
 
-    PrintWriter printWriter = resp.getWriter();
-    printWriter.println("<html>");
-    printWriter.println("<h1>Hello World!</h1>");
-    printWriter.println("</html>");
-    printWriter.close();
   }
-
 
 }
