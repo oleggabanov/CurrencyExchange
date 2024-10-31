8e464c641bd0e3468a60341919e88d347b2a5c88
@oleggabanov <oleg.gabanov@bk.ru>
2024-10-31 23:42:33 +0300

    add currency dao

---
+++ src/main/java/com/move/model/dao/CurrencyDao.java
@@ -0,0 +1,16 @@
+package com.move.model.dao;
+
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+
+@AllArgsConstructor
+public class CurrencyDao {
+  private Connection connection;
+
+
+
+
+
+
+}
