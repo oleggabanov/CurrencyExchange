040297176301f5ba458674994a64bf5c6d2681fb
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-01 14:05:17 +0300

    add builder for convenience

--- src/main/java/com/move/model/CurrencyResponse.java
+++ src/main/java/com/move/model/CurrencyResponse.java
@@ -1,10 +1,14 @@
 package com.move.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CurrencyResponse {
 
   private int id;
