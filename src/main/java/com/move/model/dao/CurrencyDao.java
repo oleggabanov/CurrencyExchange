2ee035e2d3ec4185a3f1e4e7c7fe63174fee354f
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-01 14:04:24 +0300

    get all currencies from db

--- src/main/java/com/move/model/dao/CurrencyDao.java
+++ src/main/java/com/move/model/dao/CurrencyDao.java
@@ -1,16 +1,47 @@
 package com.move.model.dao;
 
+import com.move.model.CurrencyResponse;
+import com.move.resource.ConnectionDatabase;
 import lombok.AllArgsConstructor;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@AllArgsConstructor
+
 public class CurrencyDao {
-  private Connection connection;
+
+  public List<CurrencyResponse> getCurrencies() {
+    List<CurrencyResponse> currencies = new ArrayList<>();
+    String sqlQuery = "select * from currencies;";
+    Connection connection;
+    Statement statement;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      connection = ConnectionDatabase.getConnection();
+      connection.setAutoCommit(false);
+      statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery(sqlQuery);
+      while (resultSet.next()) {
+        CurrencyResponse currencyResponse = CurrencyResponse.builder()
+                .id(resultSet.getInt("id"))
+                .code(resultSet.getString("code"))
+                .fullName(resultSet.getString("full_name"))
+                .sign(resultSet.getString("sign"))
+                .build();
+
+        currencies.add(currencyResponse);
+      }
+
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
 
 
-
-
+    return currencies;
+  }
 
 
 }
