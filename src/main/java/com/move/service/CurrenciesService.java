c545ca7a84a9d25e0a53e6374a2c48510b8dd0e0
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-01 16:25:00 +0300

    get all currencies method

--- src/main/java/com/move/service/CurrenciesService.java
+++ src/main/java/com/move/service/CurrenciesService.java
@@ -1,8 +1,28 @@
 package com.move.service;
 
-public class CurrenciesService {
-  
+import com.move.model.CurrencyResponse;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurrenciesService {
+
+  public List<CurrencyResponse> getCurrencies(ResultSet resultSet) throws SQLException {
+    List<CurrencyResponse> currencies = new ArrayList<>();
+    while (resultSet.next()) {
+      CurrencyResponse currencyResponse = CurrencyResponse.builder()
+              .id(resultSet.getInt("id"))
+              .code(resultSet.getString("code"))
+              .fullName(resultSet.getString("full_name"))
+              .sign(resultSet.getString("sign"))
+              .build();
+
+      currencies.add(currencyResponse);
+    }
+    return currencies;
+  }
 
 
 }
