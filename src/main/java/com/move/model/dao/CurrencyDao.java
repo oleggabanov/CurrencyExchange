fcf40a60753a6a10bd8a4b43bf107b49f841ae6d
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-03 03:39:23 +0300

    find by code and add currency to db methods

--- src/main/java/com/move/model/dao/CurrencyDao.java
+++ src/main/java/com/move/model/dao/CurrencyDao.java
@@ -1,29 +1,67 @@
 package com.move.model.dao;
 
+import com.move.dto.CurrencyDto;
 import com.move.resource.ConnectionDatabase;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class CurrencyDao {
 
-  public ResultSet getCurrenciesFromDB() {
+  private Connection connection;
+
+  public CurrencyDao() {
+    connection = ConnectionDatabase.getConnection();
+  }
+
+  public ResultSet findCurrenciesFromDB() {
     String sqlQuery = "select * from currencies;";
-    Connection connection;
     Statement statement;
     ResultSet resultSet;
     try {
-      Class.forName("org.sqlite.JDBC");
-      connection = ConnectionDatabase.getConnection();
       connection.setAutoCommit(false);
       statement = connection.createStatement();
       resultSet = statement.executeQuery(sqlQuery);
-    } catch (ClassNotFoundException | SQLException e) {
+    } catch (SQLException e) {
       throw new RuntimeException(e);
     }
     return resultSet;
   }
+
+  public ResultSet findCurrencyByCodeFromDB(String currencyCode) {
+    ResultSet resultSet;
+    try {
+      connection.setAutoCommit(false);
+      PreparedStatement preparedStatement = connection.prepareStatement("select * from currencies where code = (?);");
+      preparedStatement.setString(1, currencyCode);
+      resultSet = preparedStatement.executeQuery();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+    return resultSet;
+  }
+
+  public ResultSet addCurrencyToDB(CurrencyDto newCurrency) {
+    ResultSet resultSet = null;
+    String currencyCode = newCurrency.code();
+    String fullName = newCurrency.fullName();
+    String sign = newCurrency.sign();
+    try {
+      connection.setAutoCommit(false);
+      PreparedStatement preparedStatement = connection.prepareStatement("insert into currencies (code, full_name, sign) values (?,?,?);");
+      preparedStatement.setString(1, currencyCode);
+      preparedStatement.setString(2, fullName);
+      preparedStatement.setString(3, sign);
+      int isCurrencyAdded = preparedStatement.executeUpdate();
+      preparedStatement.executeQuery();
+      if (isCurrencyAdded != 0) {
+       resultSet = findCurrencyByCodeFromDB(currencyCode);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return resultSet;
+  }
+
 
 }
