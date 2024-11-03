01f0bd0fcedbca6cf256446cb2078ab986287d83
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-03 03:40:48 +0300

    post method to insert new currency

--- src/main/java/com/move/servlet/CurrenciesController.java
+++ src/main/java/com/move/servlet/CurrenciesController.java
@@ -2,25 +2,42 @@ package com.move.servlet;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.move.model.CurrencyResponse;
+import com.move.service.CurrenciesService;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet("/currencies")
 public class CurrenciesController extends HttpServlet {
 
-  private ObjectMapper mapper = new ObjectMapper();
+  private ObjectMapper objectMapper = new ObjectMapper();
+  private CurrenciesService currenciesService = new CurrenciesService();
+
 
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    resp.setContentType("application/json");
-    CurrencyResponse currencyResponse = new CurrencyResponse(1, "RUB", "Russian Ruble", "₽");
-    mapper.writeValue(resp.getOutputStream(), currencyResponse);
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    objectMapper.writerWithDefaultPrettyPrinter()
+            .writeValue(response.getWriter(), currenciesService.getCurrencies());
+  }
 
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    Map<String, String[]> requestParams = request.getParameterMap();
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    try {
+      objectMapper.writerWithDefaultPrettyPrinter()
+              .writeValue(response.getWriter(), currenciesService.addCurrency(requestParams));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
