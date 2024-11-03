b94b30112fe5d155316b844a48ad68c8d970df49
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-03 03:20:43 +0300

    get and add currency methods

--- create_tables.sql
+++ create_tables.sql
@@ -1,22 +1,25 @@
 create table if not exists currencies
 (
     id        integer primary key autoincrement not null,
-    code      varchar                       not null,
-    full_name varchar                       not null,
-    sign      varchar                       not null
+    code      varchar                           not null,
+    full_name varchar                           not null,
+    sign      varchar                           not null,
+    unique (code, full_name, sign)
 );
 
 create table if not exists exchange_rates
 (
-    id                 integer primary key autoincrement  not null,
-    base_currency_id   int references currencies (id) not null,
-    target_currency_id int references currencies (id) not null,
-    rate               decimal(6)                     not null
+    id                 integer primary key autoincrement not null,
+    base_currency_id   int references currencies (id)    not null,
+    target_currency_id int references currencies (id)    not null,
+    rate               decimal(6)                        not null,
+    unique (base_currency_id, target_currency_id, rate)
 );
 
-insert into currencies (code, full_name,sign)
+insert into currencies (code, full_name, sign)
 values ('USD', 'US Dollar', '$'),
-       ('EUR','Euro','€');
+       ('EUR', 'Euro', '€');
 
 insert into exchange_rates(base_currency_id, target_currency_id, rate)
-values (1,3,0.00953),(2,3,0.9263);
+values (1, 3, 0.00953),
+       (2, 3, 0.9263);
--- src/main/java/com/move/service/CurrenciesService.java
+++ src/main/java/com/move/service/CurrenciesService.java
@@ -1,15 +1,26 @@
 package com.move.service;
 
+import com.move.dto.CurrencyDto;
 import com.move.model.CurrencyResponse;
+import com.move.model.dao.CurrencyDao;
+import lombok.SneakyThrows;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class CurrenciesService {
 
-  public List<CurrencyResponse> getCurrencies(ResultSet resultSet) throws SQLException {
+  private CurrencyDao currencyDao;
+
+  public CurrenciesService() {
+    this.currencyDao = new CurrencyDao();
+  }
+
+  @SneakyThrows
+  public List<CurrencyResponse> getCurrencies() {
+    ResultSet resultSet = currencyDao.findCurrenciesFromDB();
     List<CurrencyResponse> currencies = new ArrayList<>();
     while (resultSet.next()) {
       CurrencyResponse currencyResponse = CurrencyResponse.builder()
@@ -22,6 +33,45 @@ public class CurrenciesService {
       currencies.add(currencyResponse);
     }
     return currencies;
+  }
+
+  @SneakyThrows
+  public CurrencyResponse getCurrencyByCode(String currencyCode) {
+    ResultSet resultSet = currencyDao.findCurrencyByCodeFromDB(currencyCode);
+
+    return CurrencyResponse.builder()
+            .id(resultSet.getInt("id"))
+            .code(resultSet.getString("code"))
+            .fullName(resultSet.getString("full_name"))
+            .sign(resultSet.getString("sign"))
+            .build();
+  }
+
+  @SneakyThrows
+  public CurrencyResponse addCurrency(Map<String, String[]> requestParams) {
+    ResultSet resultSet;
+    CurrencyResponse currencyResponse = null;
+    String code = requestParams.get("code")[0];
+    String fullName = requestParams.get("full_name")[0];
+    String sign = requestParams.get("sign")[0];
+    if (code == null || fullName == null || sign == null) {
+      throw new Exception();
+    } else {
+     CurrencyDto currencyDto = CurrencyDto.builder()
+              .code(code)
+              .fullName(fullName)
+              .sign(sign)
+              .build();
+
+       resultSet = currencyDao.addCurrencyToDB(currencyDto);
+       currencyResponse = CurrencyResponse.builder()
+               .id(resultSet.getInt("id"))
+               .code(resultSet.getString("code"))
+               .fullName(resultSet.getString("full_name"))
+               .sign(resultSet.getString("sign"))
+               .build();
+    }
+    return currencyResponse;
   }
 
 
