d5f0af8ae669ad7a57ea98c392e3efbd1dc08c7c
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-04 00:34:31 +0300

    add custom web filter to avoid boilerplate code

---
+++ src/main/java/com/move/utils/CustomWebFilter.java
@@ -0,0 +1,27 @@
+package com.move.utils;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "CustomWebFilter", value = "/*")
+public class CustomWebFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest servletRequest,
+                       ServletResponse servletResponse,
+                       FilterChain filterChain) throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+    HttpServletResponse response = (HttpServletResponse) servletResponse;
+    response.setContentType("application/json; charset=utf-8");
+    filterChain.doFilter(request, response);
+  }
+
+}
