c32c0b9a7683b90bd6ffeaa9f5477fd5c7df8d4d
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-07 23:39:48 +0300

    add method to handle exchange rate updates

--- src/main/java/com/move/servlet/exchangeRate/ExchangeRateController.java
+++ src/main/java/com/move/servlet/exchangeRate/ExchangeRateController.java
@@ -1,6 +1,7 @@
 package com.move.servlet.exchangeRate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.move.model.response.ExchangeRateResponse;
 import com.move.service.ExchangeRatesService;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,28 +9,43 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 @WebServlet("/exchangeRate/*")
 public class ExchangeRateController extends HttpServlet {
 
   private ObjectMapper objectMapper = new ObjectMapper();
   private ExchangeRatesService exchangeRatesService = new ExchangeRatesService();
+
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) {
     String requestURI = request.getRequestURI();
     String exchangeRateCodes = requestURI.split("/")[2];
 
-    String firstCode = exchangeRateCodes.substring(0,3);
+    String firstCode = exchangeRateCodes.substring(0, 3);
     String secondCode = exchangeRateCodes.substring(3);
 
-    exchangeRatesService.getExchangeRate(firstCode,secondCode);
+    ExchangeRateResponse exchangeRateResponse = exchangeRatesService.getExchangeRate(firstCode, secondCode);
     try {
-      objectMapper.writeValue(response.getWriter(),firstCode + " : " + secondCode);
+      objectMapper.writerWithDefaultPrettyPrinter()
+              .writeValue(response.getWriter(), exchangeRateResponse);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
 
   }
 
+  @Override
+  protected void doPatch(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String requestURI = request.getRequestURI();
+    String exchangeRateCodes = requestURI.split("/")[2];
+    String firstCode = exchangeRateCodes.substring(0, 3);
+    String secondCode = exchangeRateCodes.substring(3);
+    BigDecimal rate = new BigDecimal(request.getParameter("rate"));
+
+    ExchangeRateResponse exchangeRateResponse = exchangeRatesService.updateExchangeRate(firstCode,secondCode,rate);
+
+    objectMapper.writerWithDefaultPrettyPrinter().writeValue(response.getWriter(),exchangeRateResponse);
+  }
 
 }
