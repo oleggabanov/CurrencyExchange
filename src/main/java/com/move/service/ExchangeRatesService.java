856b66f9cdccc8cf60bd37bc1bbcb1465ad331ea
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-07 17:37:23 +0300

    addExchangeRate method

--- src/main/java/com/move/service/ExchangeRatesService.java
+++ src/main/java/com/move/service/ExchangeRatesService.java
@@ -1,5 +1,6 @@
 package com.move.service;
 
+import com.move.dto.ExchangeRateDto;
 import com.move.model.dao.CurrenciesDao;
 import com.move.model.response.CurrencyResponse;
 import com.move.model.response.ExchangeRateResponse;
@@ -28,8 +29,8 @@ public class ExchangeRatesService {
     while (resultSet.next()) {
       int baseCurrencyId = resultSet.getInt("base_currency_id");
       int targetCurrencyId = resultSet.getInt("target_currency_id");
-      CurrencyResponse baseCurrency = getCurrency(currenciesDao.findCurrencyByIdFromDB(baseCurrencyId));
-      CurrencyResponse targetCurrency = getCurrency(currenciesDao.findCurrencyByIdFromDB(targetCurrencyId));
+      CurrencyResponse baseCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByIdFromDB(baseCurrencyId));
+      CurrencyResponse targetCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByIdFromDB(targetCurrencyId));
       ExchangeRateResponse exchangeRateResponse = ExchangeRateResponse.builder()
               .id(resultSet.getInt("id"))
               .baseCurrency(baseCurrency)
@@ -42,7 +43,7 @@ public class ExchangeRatesService {
     return exchangeRates;
   }
 
-  public CurrencyResponse getCurrency(ResultSet resultSet) throws SQLException {
+  public CurrencyResponse getCurrencyFromResultSet(ResultSet resultSet) throws SQLException {
     return CurrencyResponse.builder()
             .id(resultSet.getInt("id"))
             .code(resultSet.getString("code"))
@@ -54,22 +55,42 @@ public class ExchangeRatesService {
   @SneakyThrows
   public ExchangeRateResponse getExchangeRate(String baseCurrencyCode, String targetCurrencyCode) {
     ExchangeRateResponse exchangeRateResponse;
-    CurrencyResponse baseCurrency = getCurrency(currenciesDao.findCurrencyByCodeFromDB(baseCurrencyCode));
-    CurrencyResponse targetCurrency = getCurrency(currenciesDao.findCurrencyByCodeFromDB(targetCurrencyCode));
+    CurrencyResponse baseCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByCodeFromDB(baseCurrencyCode));
+    CurrencyResponse targetCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByCodeFromDB(targetCurrencyCode));
     ResultSet resultSet = exchangeRatesDao.findExchangeRateByCurrencyId(baseCurrency.getId(), targetCurrency.getId());
     BigDecimal rate = resultSet.getBigDecimal("rate");
     if (resultSet.getBigDecimal("rate") == null) {
       ResultSet resultSet2 = exchangeRatesDao.findExchangeRateByCurrencyId(targetCurrency.getId(), baseCurrency.getId());
       if (resultSet2.getBigDecimal("rate") != null) {
-        rate = new BigDecimal(1.0).divide(resultSet2.getBigDecimal("rate"),10,BigDecimal.ROUND_HALF_UP);
+        rate = new BigDecimal(1.0).divide(resultSet2.getBigDecimal("rate"), 10, BigDecimal.ROUND_HALF_UP);
       }
     }
-      exchangeRateResponse = ExchangeRateResponse.builder()
-              .id(resultSet.getInt("id"))
-              .baseCurrency(baseCurrency)
-              .targetCurrency(targetCurrency)
-              .rate(rate)
-              .build();
+    exchangeRateResponse = ExchangeRateResponse.builder()
+            .id(resultSet.getInt("id"))
+            .baseCurrency(baseCurrency)
+            .targetCurrency(targetCurrency)
+            .rate(rate)
+            .build();
+    return exchangeRateResponse;
+  }
+
+  @SneakyThrows
+  public ExchangeRateResponse addExchangeRate(String baseCurrencyCode, String targetCurrencyCode, BigDecimal rate) {
+    CurrencyResponse baseCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByCodeFromDB(baseCurrencyCode));
+    CurrencyResponse targetCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByCodeFromDB(targetCurrencyCode));
+    ExchangeRateDto exchangeRateDto = ExchangeRateDto.builder()
+            .baseCurrencyId(baseCurrency.getId())
+            .targetCurrencyId(targetCurrency.getId())
+            .rate(rate)
+            .build();
+    ResultSet resultSet = exchangeRatesDao.addExchangeRateToDB(exchangeRateDto);
+
+    ExchangeRateResponse exchangeRateResponse = ExchangeRateResponse.builder()
+            .id(resultSet.getInt("id"))
+            .baseCurrency(baseCurrency)
+            .targetCurrency(targetCurrency)
+            .rate(resultSet.getBigDecimal("rate"))
+            .build();
     return exchangeRateResponse;
   }
 }
