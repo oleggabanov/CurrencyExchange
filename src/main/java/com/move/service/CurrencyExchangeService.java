177f15786d34d3978a8c84990f43c86203a3f130
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-08 00:22:59 +0300

    add service to convert amounts from one currency to another by exchange rate

---
+++ src/main/java/com/move/service/CurrencyExchangeService.java
@@ -0,0 +1,34 @@
+package com.move.service;
+
+import com.move.model.response.CurrencyExchangeResponse;
+import com.move.model.response.ExchangeRateResponse;
+import lombok.SneakyThrows;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchangeService {
+
+  private ExchangeRatesService exchangeRatesService;
+
+  public CurrencyExchangeService() {
+    this.exchangeRatesService = new ExchangeRatesService();
+  }
+
+  @SneakyThrows
+  public CurrencyExchangeResponse convertCurrency(String baseCurrencyCode, String targetCurrencyCode, BigDecimal amount) {
+    ExchangeRateResponse exchangeRateResponse = exchangeRatesService.getExchangeRate(baseCurrencyCode, targetCurrencyCode);
+    BigDecimal rate = exchangeRateResponse.getRate();
+    BigDecimal convertedAmount = amount.multiply(rate);
+
+    CurrencyExchangeResponse currencyExchangeResponse = CurrencyExchangeResponse.builder()
+            .baseCurrency(exchangeRateResponse.getBaseCurrency())
+            .targetCurrency(exchangeRateResponse.getTargetCurrency())
+            .rate(rate)
+            .amount(amount)
+            .convertedAmount(convertedAmount)
+            .build();
+
+
+    return currencyExchangeResponse;
+  }
+}
