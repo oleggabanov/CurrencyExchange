7c6652174c29782a675ef80089744af4dbc8d90e
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-09 02:35:21 +0300

    get straight, reverse or cross exchange rate method

--- src/main/java/com/move/service/ExchangeRatesService.java
+++ src/main/java/com/move/service/ExchangeRatesService.java
@@ -49,19 +49,58 @@ public class ExchangeRatesService {
 
   @SneakyThrows
   public ExchangeRateResponse getExchangeRate(String baseCurrencyCode, String targetCurrencyCode) {
-    ExchangeRateResponse exchangeRateResponse;
     CurrencyResponse baseCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByCodeFromDB(baseCurrencyCode));
     CurrencyResponse targetCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByCodeFromDB(targetCurrencyCode));
+    CurrencyResponse dollarCurrency = getCurrencyFromResultSet(currenciesDao.findCurrencyByCodeFromDB("USD"));
     ResultSet resultSet = exchangeRatesDao.findExchangeRateByCurrenciesId(baseCurrency.getId(), targetCurrency.getId());
     BigDecimal rate = resultSet.getBigDecimal("rate");
-    if (resultSet.getBigDecimal("rate") == null) {
-      ResultSet resultSet2 = exchangeRatesDao.findExchangeRateByCurrenciesId(targetCurrency.getId(), baseCurrency.getId());
-      if (resultSet2.getBigDecimal("rate") != null) {
-        rate = new BigDecimal(1.0).divide(resultSet2.getBigDecimal("rate"), 7, BigDecimal.ROUND_HALF_UP);
-      }
+    if (rate != null) {
+      int id = resultSet.getInt("id");
+      return getExchangeRateResponse(id, baseCurrency, targetCurrency, rate);
     }
-    exchangeRateResponse = ExchangeRateResponse.builder()
-            .id(resultSet.getInt("id"))
+
+    resultSet = exchangeRatesDao.findExchangeRateByCurrenciesId(targetCurrency.getId(), baseCurrency.getId());
+    rate = resultSet.getBigDecimal("rate");
+    if (rate != null) {
+      rate = BigDecimal.valueOf(1).divide(rate, 7, BigDecimal.ROUND_HALF_UP);
+      ExchangeRateDto exchangeRateDto = ExchangeRateDto.builder()
+              .baseCurrencyId(baseCurrency.getId())
+              .targetCurrencyId(targetCurrency.getId())
+              .rate(rate)
+              .build();
+      ResultSet savedExchangeRate = exchangeRatesDao.addExchangeRateToDB(exchangeRateDto);
+      int id = savedExchangeRate.getInt("id");
+      return getExchangeRateResponse(id, baseCurrency, targetCurrency, rate);
+    }
+
+    ResultSet baseResultSet = exchangeRatesDao.findExchangeRateByCurrenciesId(baseCurrency.getId(), dollarCurrency.getId());
+    ResultSet targetResultSet = exchangeRatesDao.findExchangeRateByCurrenciesId(dollarCurrency.getId(), targetCurrency.getId());
+
+    BigDecimal firstRate = baseResultSet.getBigDecimal("rate");
+    BigDecimal secondRate = targetResultSet.getBigDecimal("rate");
+
+    if (firstRate == null) {
+      baseResultSet = exchangeRatesDao.findExchangeRateByCurrenciesId(dollarCurrency.getId(), baseCurrency.getId());
+      firstRate = BigDecimal.valueOf(1).divide(baseResultSet.getBigDecimal("rate"), 7, BigDecimal.ROUND_HALF_UP);
+    }
+    if (secondRate == null) {
+      targetResultSet = exchangeRatesDao.findExchangeRateByCurrenciesId(targetCurrency.getId(), dollarCurrency.getId());
+      secondRate = BigDecimal.valueOf(1).divide(targetResultSet.getBigDecimal("rate"), 7, BigDecimal.ROUND_HALF_UP);
+    }
+
+    rate = firstRate.multiply(secondRate);
+    ExchangeRateDto exchangeRateDto = ExchangeRateDto.builder()
+            .baseCurrencyId(baseCurrency.getId())
+            .targetCurrencyId(targetCurrency.getId())
+            .rate(rate)
+            .build();
+    ResultSet savedExchangeRate = exchangeRatesDao.addExchangeRateToDB(exchangeRateDto);
+    return getExchangeRateResponse(savedExchangeRate.getInt("id"), baseCurrency, targetCurrency, rate);
+  }
+
+  private ExchangeRateResponse getExchangeRateResponse(int id, CurrencyResponse baseCurrency, CurrencyResponse targetCurrency, BigDecimal rate) throws SQLException {
+    ExchangeRateResponse exchangeRateResponse = ExchangeRateResponse.builder()
+            .id(id)
             .baseCurrency(baseCurrency)
             .targetCurrency(targetCurrency)
             .rate(rate)
