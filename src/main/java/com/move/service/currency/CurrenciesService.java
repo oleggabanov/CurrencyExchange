2907be905419bce835cd09999961950d14baa81f
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-12 23:46:40 +0300

    remove sneaky throws, params check and collapse redundant code

--- src/main/java/com/move/service/currency/CurrenciesService.java
+++ src/main/java/com/move/service/currency/CurrenciesService.java
@@ -1,51 +1,40 @@
 package com.move.service.currency;
 
-import com.move.dao.CurrenciesDao;
+import com.move.context.AppContext;
+import com.move.dao.CurrencyDao;
 import com.move.dto.CurrencyDto;
 import com.move.model.Currency;
-import lombok.SneakyThrows;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public class CurrenciesService {
 
-  private CurrenciesDao currenciesDao;
+  private CurrencyDao currencyDao;
 
   public CurrenciesService() {
-    this.currenciesDao = new CurrenciesDao();
+    this.currencyDao = AppContext.getInstance().getCurrencyDao();
   }
 
-  @SneakyThrows
   public List<Currency> getCurrencies() {
-    return currenciesDao.findAll();
+    return currencyDao.findAll();
   }
 
-  @SneakyThrows
   public Currency getCurrencyByCode(String currencyCode) {
-    Optional<Currency> findCurrencyByCode = currenciesDao.findCurrencyByCode(currencyCode);
-    return findCurrencyByCode.orElseThrow(() -> new RuntimeException("No such currency"));
+    return currencyDao.findByCode(currencyCode)
+            .orElseThrow(RuntimeException::new);
   }
 
-  @SneakyThrows
-  public Currency addCurrency(Map<String, String[]> requestParams) {
-    Currency currency;
-    String code = requestParams.get("code")[0];
-    String fullName = requestParams.get("full_name")[0];
-    String sign = requestParams.get("sign")[0];
-    if (code == null || fullName == null || sign == null) {
-      throw new Exception();
-    } else {
-      CurrencyDto currencyDto = CurrencyDto.builder()
-              .code(code)
-              .fullName(fullName)
-              .sign(sign)
-              .build();
+  public Currency addCurrency(CurrencyDto currencyParams) {
+    String code = currencyParams.code();
+    String fullName = currencyParams.fullName();
+    String sign = currencyParams.sign();
 
-      Optional<Currency> findCurrencyByCode = currenciesDao.addCurrencyToDB(currencyDto);
-      currency = findCurrencyByCode.orElseThrow(() -> new RuntimeException("No such currency"));
-    }
-    return currency;
+    Currency currency = Currency.builder()
+            .code(code)
+            .fullName(fullName)
+            .sign(sign)
+            .build();
+
+    return currencyDao.save(currency);
   }
 }
