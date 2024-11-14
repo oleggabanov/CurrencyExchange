68b633675ec908e83940f1e7d7251f97451baafb
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-14 03:13:27 +0300

    check request params

--- src/main/java/com/move/web/controller/exchange/ExchangeRateController.java
+++ src/main/java/com/move/web/controller/exchange/ExchangeRateController.java
@@ -1,6 +1,7 @@
-package com.move.controller.exchange;
+package com.move.web.controller.exchange;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.move.exception.ParamAbsenceException;
 import com.move.model.ExchangeRate;
 import com.move.service.exchange.ExchangeRatesService;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Map;
 
 @WebServlet("/exchangeRate/*")
 public class ExchangeRateController extends HttpServlet {
@@ -18,21 +20,19 @@ public class ExchangeRateController extends HttpServlet {
   private ExchangeRatesService exchangeRatesService = new ExchangeRatesService();
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-    String requestURI = request.getRequestURI();
-    String exchangeRateCodes = requestURI.split("/")[2];
-
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String[] requestUri = request.getRequestURI().split("/");
+    if (requestUri.length != 3 || requestUri[2].length() != 6) {
+      throw new ParamAbsenceException("Коды валют пары отсутствуют в адресе");
+    }
+    String exchangeRateCodes = requestUri[2];
     String firstCode = exchangeRateCodes.substring(0, 3);
     String secondCode = exchangeRateCodes.substring(3);
-
     ExchangeRate exchangeRate = exchangeRatesService.getExchangeRateByCurrencyCodes(firstCode, secondCode);
-    try {
-      objectMapper.writerWithDefaultPrettyPrinter()
-              .writeValue(response.getWriter(), exchangeRate);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
 
+    response.setStatus(HttpServletResponse.SC_OK);
+    objectMapper.writerWithDefaultPrettyPrinter()
+            .writeValue(response.getWriter(), exchangeRate);
   }
 
   @Override
@@ -41,10 +41,15 @@ public class ExchangeRateController extends HttpServlet {
     String exchangeRateCodes = requestURI.split("/")[2];
     String firstCode = exchangeRateCodes.substring(0, 3);
     String secondCode = exchangeRateCodes.substring(3);
-    BigDecimal rate = new BigDecimal(request.getParameter("rate"));
+    Map<String, String[]> requestParams = request.getParameterMap();
 
-    ExchangeRate exchangeRate = exchangeRatesService.updateExchangeRate(firstCode,secondCode,rate);
+    if (requestParams.size() != 1) {
+      throw new ParamAbsenceException("Отсутствует нужное поле формы");
+    }
 
+    BigDecimal rate = new BigDecimal(requestParams.get("rate")[0]);
+    ExchangeRate exchangeRate = exchangeRatesService.updateExchangeRate(firstCode, secondCode, rate);
+    response.setStatus(HttpServletResponse.SC_OK);
     objectMapper.writerWithDefaultPrettyPrinter().writeValue(response.getWriter(), exchangeRate);
   }
 
@@ -52,11 +57,9 @@ public class ExchangeRateController extends HttpServlet {
   protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String requestURI = request.getRequestURI();
     String exchangeRateCodes = requestURI.split("/")[2];
-
     String firstCode = exchangeRateCodes.substring(0, 3);
     String secondCode = exchangeRateCodes.substring(3);
 
-    objectMapper.writerWithDefaultPrettyPrinter()
-            .writeValue(response.getWriter(), exchangeRatesService.deleteExchangeRate(firstCode,secondCode));
+    exchangeRatesService.deleteExchangeRate(firstCode, secondCode);
   }
 }
