5ab04be165af542703e727d71d1b2ec7d7dfc342
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-16 02:58:49 +0300

    add @NoArgsConstructor

--- src/main/java/com/move/model/ExchangeRate.java
+++ src/main/java/com/move/model/ExchangeRate.java
@@ -3,11 +3,13 @@ package com.move.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ExchangeRate {
 
