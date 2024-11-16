633cf86aff4693fa1d4728fb524031fb15b60a18
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-16 11:51:15 +0300

    add headers and set content type

--- src/main/java/com/move/web/filter/CustomWebFilter.java
+++ src/main/java/com/move/web/filter/CustomWebFilter.java
@@ -20,7 +20,12 @@ public class CustomWebFilter implements Filter {
                        FilterChain filterChain) throws IOException, ServletException {
     HttpServletRequest request = (HttpServletRequest) servletRequest;
     HttpServletResponse response = (HttpServletResponse) servletResponse;
-    response.setContentType("application/json; charset=utf-8");
+    request.setCharacterEncoding("UTF-8");
+    response.setHeader("Accept", "text/html, application/x-www-form-urlencoded");
+    response.setContentType("application/json; charset=utf-8; application/x-www-form-urlencoded");
+    response.setHeader("Access-Control-Allow-Origin", "http://localhost");
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
     filterChain.doFilter(request, response);
   }
 
