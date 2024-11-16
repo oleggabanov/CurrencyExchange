07fe7c7588a25b7b333816ae05e5e2c71e294805
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-16 11:59:20 +0300

    rename CurrenciesService to CurrencyService

--- src/main/java/com/move/web/controller/currency/CurrencyController.java
+++ src/main/java/com/move/web/controller/currency/CurrencyController.java
@@ -3,7 +3,7 @@ package com.move.web.controller.currency;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.move.context.AppContext;
 import com.move.exception.ParamAbsenceException;
-import com.move.service.currency.CurrenciesService;
+import com.move.service.currency.CurrencyService;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class CurrencyController extends HttpServlet {
 
   private ObjectMapper objectMapper = AppContext.getInstance().getObjectMapper();
-  private CurrenciesService currenciesService = new CurrenciesService();
+  private CurrencyService currencyService = new CurrencyService();
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -27,7 +27,7 @@ public class CurrencyController extends HttpServlet {
 
     response.setStatus(HttpServletResponse.SC_OK);
     objectMapper.writerWithDefaultPrettyPrinter()
-            .writeValue(response.getWriter(), currenciesService.getCurrencyByCode(pathInfo[2]));
+            .writeValue(response.getWriter(), currencyService.getCurrencyByCode(pathInfo[pathInfo.length - 1]));
   }
 
 }
