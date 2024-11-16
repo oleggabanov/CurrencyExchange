ee29e28af1c235ca158e1e57ea055b3b8b5ea4b4
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-16 11:47:40 +0300

    length constraint

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,9 @@
 create table if not exists currencies
 (
     id        integer primary key autoincrement not null,
-    code      varchar                           not null,
-    full_name varchar                           not null,
-    sign      varchar                           not null,
+    code      varchar(3)                        not null,
+    full_name varchar(30)                       not null,
+    sign      varchar(1)                        not null,
     unique (code, full_name, sign)
 );
 
@@ -12,7 +12,7 @@ create table if not exists exchange_rates
     id                 integer primary key autoincrement not null,
     base_currency_id   int references currencies (id)    not null,
     target_currency_id int references currencies (id)    not null,
-    rate               decimal(7)                        not null,
+    rate               decimal(7) check ( rate >= 0 )    not null,
     unique (base_currency_id, target_currency_id, rate)
 );
 
