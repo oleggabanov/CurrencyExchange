836284cf6199d549d44b7cdfabb0f5642ee066ac
@oleggabanov <oleg.gabanov@bk.ru>
2024-11-27 10:49:00 +0300

    rename files

--- src/main/resources/db/V1__create_tables.sql
+++ src/main/resources/db/V1__create_tables.sql
@@ -15,12 +15,3 @@ create table if not exists exchange_rates
     rate               decimal(7) check ( rate >= 0 )    not null,
     unique (base_currency_id, target_currency_id, rate)
 );
-
-insert into currencies (code, full_name, sign)
-values ('USD', 'US Dollar', '$'),
-       ('EUR', 'Euro', '€'),
-       ('RUB', 'Russian Rubble', '₽');
-
-insert into exchange_rates(base_currency_id, target_currency_id, rate)
-values (1, 2, 0.9288),
-       (2, 1, 1.0769);
